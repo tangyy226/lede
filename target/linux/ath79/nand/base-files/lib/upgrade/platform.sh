936ddb5090c99f91383200ddc629d3c5cbb0bb72
tangyy226 <93630245+tangyy226@users.noreply.github.com>
2021-11-14 11:07:37 +0800

    Update platform.sh

--- target/linux/ath79/nand/base-files/lib/upgrade/platform.sh
+++ target/linux/ath79/nand/base-files/lib/upgrade/platform.sh
@@ -22,6 +22,9 @@ platform_do_upgrade() {
 	glinet,gl-ar750s-nor-nand)
 		nand_nor_do_upgrade "$1"
 		;;
+	glinet_gl-xe300-nor-nand)
+		nand_nor_do_upgrade "$1"
+		;;
 	*)
 		nand_do_upgrade "$1"
 		;;
